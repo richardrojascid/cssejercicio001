2449ed92f29200c3c1b3d623da9c35f589ceb4ea
cetecom <cetecom@LCV10804>
2019-03-18 22:31:04 -0300

    trabajo con jquery y array de objetos

--- lib/recepcionvacaciones.php
+++ lib/recepcionvacaciones.php
@@ -1,8 +1,18 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include 'constantes.php';
+include 'vacaciones';
+if(isset ($_SESSION[$aVacaciones])){
+    
+}
 
+echo"<pre>";
+var _dump($_POST);
+echo "</pre>";
+
+if(isset($_SESSION["aVacaciones"])){
+    $arrVacaciones=$_SESSION["aVacaciones"];
+    
+}
+
+$arrVacaciones[]=$oVacaciones;
